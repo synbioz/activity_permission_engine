e9701ad77a752d976b07653c6be27c0f3dfbcfb2
Cedric Brancourt <cedric.brancourt@gmail.com>
2016-01-28 14:46:51 +0100

    feat(configuration): add activities registry

--- lib/activity_permission_engine.rb
+++ lib/activity_permission_engine.rb
@@ -1,5 +1,34 @@
-require "activity_permission_engine/version"
+require_relative 'activity_permission_engine/version'
+require_relative 'activity_permission_engine/register_activity'
 
 module ActivityPermissionEngine
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  def self.register_activity(request)
+    RegisterActivity.new(request, self.configuration.activities_registry).call
+  end
+
+  class Configuration
+    def initialize(options={})
+      @activities_registry = options.fetch(:activities_registry, Defaults.activities_registry)
+    end
+
+    attr_accessor :activities_registry
+  end
+
+  module Defaults
+    def self.activities_registry
+      # TODO: return a simple file based registry
+    end
+  end
 end
