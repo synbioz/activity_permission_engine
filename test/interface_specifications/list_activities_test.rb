11ef73f15a77b0dc887dbd8ea41b9f59943ff074
Cedric Brancourt <cedric.brancourt@gmail.com>
2016-01-28 15:57:24 +0100

    test(list_activity): implement specs tests

--- test/interface_specifications/list_activities_test.rb
+++ test/interface_specifications/list_activities_test.rb
@@ -1,13 +1,25 @@
 require_relative '../test_helper'
 
-describe 'ActivityPermissionEngine.list_activities' do
-  it 'returns ListActivities::Response' do
-    skip 'pending'
-  end
+module ActivityPermissionEngine
+  describe 'ActivityPermissionEngine.list_activities' do
+    let(:activity_refs) { ['foo', 'bar'] }
+    let(:activity_registry) { Minitest::Mock.new.expect(:all, activity_refs) }
+    subject { ActivityPermissionEngine.list_activities }
 
-  describe 'ListActivities::Response#activities' do
-    it 'returns Array(activity_ref)' do
-      skip 'pending'
+    it 'send :all to the activity_registry' do
+      subject
+      activity_registry.verify
+    end
+
+    it 'returns ListActivities::Response' do
+      subject.must_be_kind_of ListAcitivities::Response
+    end
+
+    describe 'ListActivities::Response#activities' do
+      subject { ActivityPermissionEngine.list_activities.activities }
+      it 'returns Array(activity_ref)' do
+        subject.must_equal activity_refs
+      end
     end
   end
 end
