d4411ec64941c79ba80d8b59aaf5bcb049f96100
Cedric Brancourt <cedric.brancourt@gmail.com>
2016-02-01 00:07:07 +0100

    fix(activity_permissions_registry): add missing file

---
+++ lib/activity_permission_engine/activity_permissions_registry.rb
@@ -0,0 +1,57 @@
+module ActivityPermissionEngine
+  module ActivityPermissionsRegistry
+    module Interface
+
+      # @param [String] activity_ref
+      # @return [Boolean] true if added or false
+      def add(activity_ref)
+        raise NotImplementedError
+      end
+
+      # @return [Array(Activity)]
+      def all
+        get_all_activities.map { |activity| Activity.new(activity[:activity_ref], activity[:role_refs])}
+      end
+
+      # @param [String] activity_ref
+      # @return [Boolean] true if deleted or false
+      def del(activity_ref)
+        raise NotImplementedError
+      end
+
+      # @param [String] activity_ref
+      # @param [Array(String)] role_ref
+      # @return [Boolean] true if added or false
+      def add_role(activity_ref, role_ref)
+        raise NotImplementedError
+      end
+
+      # @param [String] activity_ref
+      # @return [Activity] the found activity or false
+      def find_by_activity_ref(activity_ref)
+        activity = get_activity_by_ref(activity_ref)
+        activity && Activity.new(activity[:activity_ref], activity[:role_refs])
+      end
+
+      private
+
+      # @return [Array(Hash)]
+      def get_all_activities
+        raise NotImplementedError
+      end
+
+      def get_activity_by_ref(activity_ref)
+        raise NotImplementedError
+      end
+    end
+
+    class Activity
+      def initialize(activity_ref, role_refs)
+        @activity_ref = activity_ref
+        @role_refs = role_refs
+      end
+
+      attr_reader(:activity_ref, :role_refs)
+    end
+  end
+end
